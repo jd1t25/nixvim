de60dbe305f5663fa37295fb05ca6453f70b1555
jd1t25 <mayureshsaitwal25@gmail.com>
2024-06-23 15:06:43 +0530

    nearly done

--- config/keys.nix
+++ config/keys.nix
@@ -23,18 +23,83 @@
       key = "jk";
       action = "<esc>";
       options = {
-        desc = "escape";
-	noremap = true;
+        desc = "Escape to normal mode";
+        noremap = true;
       };
     }
 {
-      mode = "n";
+      mode = [ "n" "i"];
       key = "JK";
       action = "<Esc>:wq<CR>";
       options = {
-        desc = "escapea and save";
-	noremap = true;
+        desc = "Save and Exit";
+        noremap = true;
       };
+    }
+        {
+      mode = [ "n" "i"];
+      key = "JJ";
+      action = "<Esc>:w<CR>";
+      options = {
+        desc = "Save the file";
+        noremap = true;
+      };
+        {
+      mode = [ "n" "i"];
+      key = "KK";
+      action = "<Esc>:qa!<CR>";
+      options = {
+        desc = "Don't Save and exit";
+        noremap = true;
+      };
+    }
+    {
+      mode = ["n" "v"];
+      key = "<C-C>";
+      action = ''"+y'';
+      options = {desc = "Copy to system clipboard";};
+    }
+    {
+      mode = ["n" "v"];
+      key = "<C-V>";
+      action = "p";
+      options = {desc = "Paste from system clipboard";};
+    }
+    {
+      mode = ["n" "v"];
+      key = "dd";
+      action = '"_dd"';
+      options = {desc = "Delete to Void";};
+    }
+    {
+      mode = ["n" "v"];
+      key = "c";
+      action = '"_c"';
+      options = {desc = "Cut to void";};
+    }
+    {
+      mode = "v";
+      key = "<";
+      action = "<gv";
+      options = {desc = "Move 1 left indent";};
+    }
+    {
+      mode = "v";
+      key = ">";
+      action = ">gv";
+      options = {desc = "Move 1 right indent";};
+    }
+    {
+      mode = "n";
+      key = "j";
+      action = "v:count == 0 ? 'gj' : 'j'";
+      options = {desc = "Dealing with word wrap"; expr = true};
+    }
+    {
+      mode = "n";
+      key = "k";
+      action = "v:count == 0 ? 'gk' : 'k'";
+      options = {desc = "Dealing with word wrap"; expr = true};
     }
   ];
  # extraConfigVim = ''
