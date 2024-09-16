1b383e0551e9e00af245a4786c068187628f348e
jd1t25 <mayureshsaitwal25@gmail.com>
2024-09-16 14:45:37 +0530

    Change format_on_save

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -1,24 +1,69 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    format_on_save = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
-    notifyOnError = true;
-    formattersByFt = {
-      liquidsoap = ["liquidsoap-prettier"];
-      html = [["prettierd" "prettier"]];
-      css = [["prettierd" "prettier"]];
-      javascript = [["prettierd" "prettier"]];
-      javascriptreact = [["prettierd" "prettier"]];
-      typescript = [["prettierd" "prettier"]];
-      typescriptreact = [["prettierd" "prettier"]];
-      python = ["black"];
-      lua = ["stylua"];
-      nix = ["alejandra"];
-      markdown = [["prettierd" "prettier"]];
-      yaml = ["yamllint" "yamlfmt"];
+    settings = {
+
+      format_on_save = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      notify_on_error = true;
+
+      formatters_by_ft = {
+        liquidsoap = [ "liquidsoap-prettier" ];
+        html = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        css = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        javascript = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        javascriptreact = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        typescript = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        typescriptreact = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        python = [ "black" ];
+        lua = [ "stylua" ];
+        nix = [ "nixfmt" ];
+        markdown = [
+          [
+            "prettierd"
+            "prettier"
+          ]
+        ];
+        yaml = [
+          "yamllint"
+          "yamlfmt"
+        ];
+        terragrunt = [
+          "hclfmt"
+        ];
+      };
     };
   };
 }
