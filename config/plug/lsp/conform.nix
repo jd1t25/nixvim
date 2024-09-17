eaea00e354942dbd4a36fc528311523ab5717577
jd1t25 <mayureshsaitwal25@gmail.com>
2024-09-17 14:05:18 +0530

    Change format_on_save

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -1,12 +1,11 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    settings = {
-
-      format_on_save = {
-        lspFallback = true;
-        timeoutMs = 500;
-      };
+    #settings = {
+    #  format_on_save = {
+    #    lspFallback = true;
+    #    timeoutMs = 500;
+    #  };
       notify_on_error = true;
 
       formatters_by_ft = {
@@ -64,6 +63,6 @@
           "hclfmt"
         ];
       };
-    };
+#    };
   };
 }
