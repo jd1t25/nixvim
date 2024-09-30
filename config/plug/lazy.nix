501dd978c6dcc8ebfe77e5593f2478b5030899f4
jd1t25 <mayureshsaitwal25@gmail.com>
2024-09-30 17:29:16 +0530

    Added Lazy

--- config/plug/lazy.nix
+++ config/plug/lazy.nix
@@ -3,15 +3,31 @@ _: {
     enable = true;
     plugins = [
       {
-        "barrett-ruth/live-server.nvim" = {
-          enabled = true;
-          cmd = [
-            "LiveServerStart"
-            "LiveServerStop"
-          ];
-          config = true;
-        };
+        repo = "barrett-ruth/live-server.nvim"; # Specify the plugin repository
+        enabled = true;
+        cmd = [
+          "LiveServerStart"
+          "LiveServerStop"
+        ]; # List of commands for lazy loading
+        config = true;
       }
     ];
   };
 }
+# plugins.lazy = {
+#   enable = true;
+#   plugins = [
+#     {
+#       "barrett-ruth/live-server.nvim" = {
+#         enabled = true;
+#         cmd = [
+#           "LiveServerStart"
+#           "LiveServerStop"
+#         ];
+#         config = true;
+#       };
+#     }
+#   ];
+# };
+# }
+
